7102f8164765ef171ede23553773c32b74e16aa2
Kuldeep Paikara <kuldeeppaikara459@gmail.com>
2025-06-25 15:24:50 +0530

    projects page added with gooey effect and decent layout

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ function loadingAnimation() {
     opacity: 0,
     // duaration : 0.1,
     stagger: 0.1,
-  });
+  },"-=0.8");
 }
 
 
@@ -122,3 +122,13 @@ gsapScrollTrigger();
 loadingAnimation();
 cursorAnime();
 
+function SheryAnime() {
+  Shery.imageEffect(".imgDiv",{
+    style:5,
+    // debug: true,
+    config:{"a":{"value":2.52,"range":[0,30]},"b":{"value":0.4,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.799994569354803},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1.37,"range":[0.1,5]},"durationIn":{"value":1.52,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":2.1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.61,"range":[0,10]},"metaball":{"value":0.4,"range":[0,2]},"discard_threshold":{"value":0.6,"range":[0,1]},"antialias_threshold":{"value":0.03,"range":[0,0.1]},"noise_height":{"value":0.44,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+    
+    gooey: true,
+  }) 
+}
+SheryAnime();
